a0680466629bfccfc57575843230b57afca57770
etozheping <krashsamara@gmail.com>
2022-09-11 02:13:22 +0400

    Решение задачи №4

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+    }
+    }
