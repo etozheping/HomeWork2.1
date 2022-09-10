af587c6384a8557b49fb7df7174203fd4061c1c3
etozheping <krashsamara@gmail.com>
2022-09-11 02:18:52 +0400

    Решение задачи №5

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        var friend = 19;
-        System.out.println(friend);
-        friend = friend + 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
+        // var friend = 19;
+        //System.out.println(friend);
+        //friend = friend + 2;
+        //System.out.println(friend);
+        //friend = friend / 7;
+        //System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
     }
